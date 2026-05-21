eda6478597c4e909098625e415c9030aeaa0d295
Александр Николашкин <nikolashkinalex@gmail.com>
2026-05-21 15:40:55 +0900

    Включил CORS для мобильного фронта

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ['http://localhost:8081', 'http://localhost:19006'],
+    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
